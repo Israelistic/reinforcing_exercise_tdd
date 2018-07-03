ccb04badc6f2ccb089129d6cfa86d25737a2e5ec
Israelistic <haguyle@gmail.com>
2018-07-03 14:48:53 -0400

    wrote methods to match the test

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+def add(num1, num2)
+    result = num1 + num2
+    return result
+end
+
+def subtract(num1, num2)
+    result =  num1 - num2
+    return result
+end
+
+
+def sum(numbers)
+    sum = 0
+    numbers.each do |number |
+        sum += number
+
+    end
+    return sum
+end
